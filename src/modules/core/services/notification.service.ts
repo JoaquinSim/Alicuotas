dfd8aef8314f1e982535f39444422cd29aaeaf8a
Joaquin Simbaña <joaquin050416@gmail.com>
2024-01-26 07:50:39 -0500

    fix bugs

--- src/modules/core/services/notification.service.ts
+++ src/modules/core/services/notification.service.ts
@@ -1,34 +1,26 @@
 // notification.service.ts
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable, OnApplicationBootstrap  } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as schedule from 'node-schedule';
-import { TimeDetailService } from './time-detail.service';
-import { LoteService } from './lote.service';
-import { UsersService } from '@auth/services';
-import { LoteDto } from '../dto/lote/lote.dto';
-import { UserDto } from '@auth/dto';
 import { MailService } from '../../mails/services/mail-service.';
 
 @Injectable()
-export class NotificationService implements OnApplicationBootstrap  {
-    constructor(private mailService: MailService) {
-      
-    }
-    onApplicationBootstrap() {
-    this.scheduleAutomaticNotification();
+export class NotificationService implements OnModuleInit {
+  constructor(private mailService: MailService) {}
+  onModuleInit() {
+    this.executeNotification();
   }
-
+  executeNotification() {
+    setInterval(() => {
+      this.scheduleAutomaticNotification();
+    }, 10000);
+  }
   private async scheduleAutomaticNotification() {
-
-
-    
-    const futureDate = new Date(); 
-    futureDate.setSeconds(futureDate.getSeconds() + 5); 
-
+    const futureDate = new Date();
+    futureDate.setSeconds(futureDate.getSeconds() + 5);
 
     const job = schedule.scheduleJob(futureDate, () => {
-      if(futureDate.getDate() > 15 && futureDate.getDate() < 30 ){
-        this.mailService.example()
+      if (futureDate.getDate() > 15 && futureDate.getDate() < 30) {
+        this.mailService.example();
       }
     });
   }
